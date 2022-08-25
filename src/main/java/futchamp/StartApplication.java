34de851d441581900837ad0b90ae7d1e06433c37
Tomas Estrada <tomasenrique@live.com>
2022-08-25 02:58:22 +0200

    Initial commit

---
+++ src/main/java/futchamp/StartApplication.java
@@ -0,0 +1,13 @@
+package futchamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+
+}
